2014a172ff55e007560307da8a934eef7d9f8756
gtamani <gtamani96@gmail.com>
2020-12-28 18:07:14 -0300

    Estreno repositorio
    Funciones de Alpaca Vantage. API gratuita de mktData EEUU

---
+++ alpacaVantage.py
@@ -0,0 +1,128 @@
+import requests, pandas as pd
+
+def get_intra(symbol,interval = "15min"):
+    """
+    DataFrame con datos OHCL intradiarios. Frecuencias admitidas: 1min, 5min, 15min, 30min, 60min
+    """
+    params = {"function": "TIME_SERIES_INTRADAY","symbol": symbol,"interval": interval ,"apikey":"YELY5RODQVJZX36J"}
+    url = "https://www.alphavantage.co/query"
+    r = requests.get(url=url, params=params)
+    data = r.json()["Time Series ("+interval+")"]
+
+    return set_df(data)
+
+
+def get_daily(symbol):
+    """
+    DataFrame con datos OHCL. No ajustado por dividendos
+    """
+    params = {"function": "TIME_SERIES_DAILY", "symbol": symbol, "apikey": "YELY5RODQVJZX36J"}
+    url = "https://www.alphavantage.co/query"
+    r = requests.get(url=url, params=params)
+    data = r.json()["Time Series (Daily)"]
+    return set_df(data)
+
+
+def set_df(data):
+    """
+    Seteo de DataFrames
+    """
+    df = pd.DataFrame.from_dict(data, orient="index")
+    df.index.name, df.columns = "Date", ["Open", "High", "Low", "Close", "Volume"]
+    df = df.sort_values("Date", ascending=True)
+    df.index = pd.to_datetime(df.index)
+    return df
+
+def get_dailyAdj(symbol):
+    """
+    Devuelve un DataFrame con la serie histórica de un activo, OHCL, Close Adjusted, Dividends, Splits
+    """
+    params = {"function": "TIME_SERIES_DAILY_ADJUSTED", "symbol": symbol, "apikey": "YELY5RODQVJZX36J",
+              "outputsize": "full"}
+    url = "https://www.alphavantage.co/query"
+    r = requests.get(url=url, params=params)
+    data = r.json()["Time Series (Daily)"]
+    df = pd.DataFrame.from_dict(data,orient="index").astype("float")
+    df.index.name, df.columns = "Date", ["Open", "High", "Low", "Close", "Adj Close", "Volume", "Div", "Split"]
+    df = df.sort_values("Date", ascending=True)
+    df.index = pd.to_datetime(df.index)
+    return df
+
+def get_splits(symbol):
+    """
+    Devuelve los últimos splits de un activo
+    """
+    df = get_dailyAdj(symbol)
+    return df.loc[df.Split != 1]
+
+def get_dividends(symbol):
+    """
+    Devuelve los últimos pagos de dividendos de un activo
+    """
+    df = get_dailyAdj(symbol)
+    return df.loc[df.Div != 0]
+
+def search(keyword):
+    """
+    Buscador de activos por palabras claves
+    """
+    params = {"function": "SYMBOL_SEARCH", "keywords": keyword, "apikey": "YELY5RODQVJZX36J"}
+    url = "https://www.alphavantage.co/query"
+    r = requests.get(url=url, params=params)
+    data = r.json()["bestMatches"]
+    for i in data:
+        print("{:13} - {} ({})".format(i["1. symbol"],i["2. name"],i["4. region"]))
+
+def last_price(symbol):
+    """
+    Último precio de un activo
+    """
+    params = {"function": "GLOBAL_QUOTE", "symbol": symbol, "apikey": "YELY5RODQVJZX36J"}
+    url = "https://www.alphavantage.co/query"
+    r = requests.get(url=url, params=params)
+    return float((r.json()["Global Quote"]["05. price"]))
+
+
+def fx(curr1,curr2):
+    """
+    Devuelve la relación de dos pares de monedas
+    """
+    params = {"function": "CURRENCY_EXCHANGE_RATE", "from_currency": curr1, "to_currency": curr2, "apikey": "YELY5RODQVJZX36J"}
+    url = "https://www.alphavantage.co/query"
+    r = requests.get(url=url, params=params)
+    return round(float(r.json()['Realtime Currency Exchange Rate']["5. Exchange Rate"]),2)
+
+
+def crypto_rate(symbol):
+    """
+    Devuelve el rating FCAS (Fundamental Crypto Asset Score)
+    """
+    params = {"function": "CRYPTO_RATING", "symbol": symbol, "apikey": "YELY5RODQVJZX36J"}
+    url = "https://www.alphavantage.co/query"
+    r = requests.get(url=url, params=params)
+    return int(r.json()["Crypto Rating (FCAS)"]['4. fcas score'])
+
+def ema(symbol,time_period,last = True,series_type = "close",interval="daily"):
+    """
+    Media Móvil Exponencial
+
+    :param last: True devuelve la EMA para la última cotización, False devuelve EMA para una serie historica
+    :param time_period: Frecuencia de la media móvil
+    :param series_type: Datos tomados para el cálculo del indicador ("close","open","high","low"
+    :param interval: Número de datapoints. Solo integers
+    """
+    params = {"function": "EMA", "symbol": symbol, "interval": interval, "apikey": "YELY5RODQVJZX36J",
+              "time_period": time_period, "series_type": series_type }
+    url = "https://www.alphavantage.co/query"
+    r = requests.get(url=url, params=params)
+
+    if last:
+        return float(r.json()['Technical Analysis: EMA'][r.json()['Meta Data']['3: Last Refreshed']]["EMA"])
+    else:
+        data = r.json()['Technical Analysis: EMA']
+        df = pd.DataFrame.from_dict(data,orient="index")
+        df.index.name = "Date"
+        df.index = pd.to_datetime(df.index)
+        return df
+
+
